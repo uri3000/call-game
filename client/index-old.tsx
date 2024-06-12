7c795a97673f59bcab99278a458068a12d3e8ab2
Uri Abramson <abramson.uri@gmail.com>
2024-06-13 00:52:32 +0300

    aasd

--- client/index-old.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find root element');
-
-const root = createRoot(rootElement);
-root.render(<App />);
